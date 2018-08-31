46216f776cb7b182dfe60e8f8a026f09dd4f54ba
ntregillus <ntregillus@gmail.com>
2018-08-31 09:59:41 -0600

    initial commit for cafe au play

---
+++ src/app.js
@@ -0,0 +1,10 @@
+//import './utils.js' ;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+console.log('App.js is running.');
+
+
+ReactDOM.render(<p>boiler plate shit</p>, document.getElementById('app'));
